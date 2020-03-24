b126d5e4c8f4026ad4c6c43342b25fcd6de8084c
Luka Radovanovic <lukadlm97@gmail.com>
2020-03-24 18:32:56 +0100

    created responsive nav bar with toggle

---
+++ src/pages/Rooms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Rooms() {
+    return (
+        <div>
+            hello from rooms page
+        </div>
+    )
+}
+
+export default Rooms
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class SingleRoom extends Component {
+    render() {
+        return (
+            <div>
+                hello from single room
+            </div>
+        )
+    }
+}
+export default SingleRoom
